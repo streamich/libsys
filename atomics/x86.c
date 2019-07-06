e852fb64a2a044fc82c90bae336d59fb9f3e4db2
streamich <streamich@gmail.com>
2019-07-06 14:50:32 +0200

    fix: 🐛 use correct cmpxchg32 result type

--- atomics/x86.c
+++ atomics/x86.c
@@ -1,7 +1,7 @@
 #include <sys/types.h>
 
 static inline int32_t cmpxchg32(int32_t* ptr, int32_t oldval, int32_t newval) {
-  int out;
+  int32_t out;
   __asm__ __volatile__ ("lock; cmpxchg %2, %1;"
         : "=a" (out), "+m" (*(volatile int32_t*) ptr)
         : "r" (newval), "0" (oldval)
